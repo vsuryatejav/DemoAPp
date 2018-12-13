9ed551fcc8d7c6c890ad2c8204f1d891ea44369b
suryateja <suryateja@surya>
2018-12-13 22:12:21 +0530

    asd

--- src/com/app/Demo1.java
+++ src/com/app/Demo1.java
@@ -5,15 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World....");
-		System.out.println("Hello World....");
-
-		System.out.println("Hello World....");
-
-		System.out.println("Hello World....");
-
-		System.out.println("Hello World....");
-
-		System.out.println("Hello World....");
+		
 
 	}
 }
